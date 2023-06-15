40ceaf68cf9e8111fb7c021e0a0e73f71c224c5b
rivasr <rivasr@gitlab.buildone.co>
2023-06-15 03:02:13 -0500

    Initial commit

---
+++ src/P1.h
@@ -0,0 +1,16 @@
+//
+// Created by rudri on 10/11/2020.
+//
+
+#ifndef PROG3_PC2_TEO2_V2023_1_P1_H
+#define PROG3_PC2_TEO2_V2023_1_P1_H
+
+#include "join_containers.h"
+
+void question_1_1();
+void question_1_2();
+void question_1_3();
+void question_1_4();
+void question_1_5();
+
+#endif //PROG3_PC2_TEO2_V2023_1_P1_H
---
+++ src/P2.h
@@ -0,0 +1,16 @@
+//
+// Created by rudri on 10/11/2020.
+//
+
+#ifndef PROG3_PC2_TEO2_V2023_1_P2_H
+#define PROG3_PC2_TEO2_V2023_1_P2_H
+
+#include "parallel_variance.h"
+
+void question_2_1();
+void question_2_2();
+void question_2_3();
+void question_2_4();
+void question_2_5();
+
+#endif //PROG3_PC2_TEO2_V2023_1_P2_H
